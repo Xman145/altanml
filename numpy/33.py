d09a134a174fd5bbbaa7c0d89b0b7e09694eb934
Student-8 <you@example.com>
2019-03-20 19:56:09 +0900

    33 34

---
+++ numpy/33.py
@@ -0,0 +1,7 @@
+import numpy as np
+yesterday = np.datetime64('today', 'D') - np.timedelta64(1, 'D')
+today     = np.datetime64('today', 'D')
+tomorrow  = np.datetime64('today', 'D') + np.timedelta64(1, 'D')
+print(yesterday)
+print(today)
+print(tomorrow)
